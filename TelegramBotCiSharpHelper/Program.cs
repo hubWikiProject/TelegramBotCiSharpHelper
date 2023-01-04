02ec985bce3447eaeb86b80a8a8bf7e58562df9b
hub Wiki Project <stukalov.blog@gmail.com>
2023-01-04 16:41:11 +0200

    f

--- TelegramBotCiSharpHelper/Program.cs
+++ TelegramBotCiSharpHelper/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            TelegramBotHelper telegramBotHelper = new TelegramBotHelper(token: "5950929953:AAGES3C_z8us1StvDJa02cWoQW4i3f827F");
+            TelegramBotHelper telegramBotHelper = new TelegramBotHelper(token: "5950929953:AAGES3C_z8us1StvDJa02cWoQW4i3f827F1");
             telegramBotHelper.GetUpdates();
 
             Console.ReadLine();
